464c12dc67e26e7f1b833ac9f0b1f3012835bec9
Irfan Maulana <irfanmaulana695@gmail.com>
2021-03-04 14:54:42 +0700

    update occupation data

--- database/seeders/OccupationsSeeder.php
+++ database/seeders/OccupationsSeeder.php
@@ -15,8 +15,20 @@ class OccupationsSeeder extends Seeder
      */
     public function run()
     {
-        Occupations::Create(['name' => 'Software Engineer']);
-        Occupations::Create(['name' => 'Database Engineer']);
+        Occupations::Create(['name' => 'Business Analyst']);
+        Occupations::Create(['name' => 'Cloud Computing Engineer']);
         Occupations::Create(['name' => 'Data Scientist']);
+        Occupations::Create(['name' => 'Database Administrator']);
+        Occupations::Create(['name' => 'Database Engineer']);
+        Occupations::Create(['name' => 'IT Infrastructure']);
+        Occupations::Create(['name' => 'IT Security']);
+        Occupations::Create(['name' => 'Product Owner']);
+        Occupations::Create(['name' => 'Project Manager']);
+        Occupations::Create(['name' => 'Quality Assurance']);
+        Occupations::Create(['name' => 'Software Engineer']);
+        Occupations::Create(['name' => 'System Analyst']);
+        Occupations::Create(['name' => 'UI/UX Designer']);
+        Occupations::Create(['name' => 'Web Engineer']);
+        Occupations::Create(['name' => 'Other']);
     }
 }
